424888035ec13c22db08f2941855d79113cca94e
TeoVincent <artur.wincenciak@gmail.com>
2022-08-07 20:01:27 +0200

    Add info prop of response data

--- AspNetCoreDockerHubAzurePlayground/WeatherForecast.cs
+++ AspNetCoreDockerHubAzurePlayground/WeatherForecast.cs
@@ -9,4 +9,11 @@ public class WeatherForecast
     public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
 
     public string? Summary { get; set; }
+
+    public object Info => new
+    {
+        AppName = nameof(AspNetCoreDockerHubAzurePlayground),
+        Version = "0.0.0.1",
+        Contributor = "TeoVincent"
+    };
 }
